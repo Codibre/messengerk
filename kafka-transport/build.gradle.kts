db9f962b215420a7eb3735ae39ac6826043382d9
Gustavo Andrade Ferreira <gustavo.andrade@maxmilhas.com.br>
2023-05-22 17:01:26 -0300

    build: updates kafka-transport to serialization 1.8.21

--- kafka-transport/build.gradle.kts
+++ kafka-transport/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.github.codibre.common-conventions")
-    kotlin("plugin.serialization") version "1.8.10"
+    kotlin("plugin.serialization") version "1.8.21"
 }
 
 dependencies {
